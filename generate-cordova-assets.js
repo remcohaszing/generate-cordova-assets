ace5f141f2a704b0647c87967dc1d4238bed6120
Remco Haszing <remcohaszing@gmail.com>
2016-12-13 19:04:03 +0100

    refactor: use popular packages to convert files
    This makes the result more reliable. Also this removes the need for a
    PhantomJS script.

--- generate-cordova-assets.js
+++ generate-cordova-assets.js
@@ -15,39 +15,39 @@
 
 const path = require('path');
 
-const phantomjs = require('phantomjs-prebuilt');
-
-const util = require('./lib/util');
+const fs = require('pn/fs');
+const processImage = require('./lib/process-image');
 const transparentIcons = require('./lib/transparent-icons');
+const util = require('./lib/util');
+const writeSafe = require('./lib/write-safe');
 
 
 module.exports = (context) => {
   const ConfigParser = context.requireCordovaModule('cordova-common').ConfigParser;
-  const superspawn = context.requireCordovaModule('cordova-common').superspawn;
 
   const config = new ConfigParser(path.resolve(context.opts.projectRoot, 'config.xml'));
 
   const backgroundColor = config.getPreference('IconBackgroundColor') || 'white';
-  const sourceFile = config.getPreference('IconSource');
+  const iconSource = config.getPreference('IconSource');
 
   const images = [];
   context.opts.cordova.platforms.forEach((platform) => {
     images.push(...config.getIcons(platform)
         .map(icon => util.processIconDensity(icon))
         .map((icon) => {
-          if (transparentIcons.indexOf(platform) === -1) {
-            return util.setBackground(icon, backgroundColor);
+          if (transparentIcons.indexOf(platform) !== -1) {
+            return icon;
           }
-          return util.setBackground(icon, 'rgba(0, 0, 0, 0)');
+          return util.setBackground(icon, backgroundColor);
         }));
     images.push(...config.getSplashScreens(platform)
         .map(splashScreen => util.processSplashScreenDensity(splashScreen))
         .map(splashScreen => util.setBackground(splashScreen, backgroundColor)));
   });
   console.log(`Generating ${images.length} images...`);
-  return superspawn.spawn(phantomjs.path, [
-    path.resolve(__dirname, 'phantomjs-generate-assets.js'),
-    path.resolve(context.opts.projectRoot, sourceFile),
-    JSON.stringify(images),
-  ], { stdio: 'inherit' });
+  return Promise.all(images.map(({ width, height, src, bgColor }) =>
+    fs.readFile(iconSource)
+      .then(sourceBuffer => processImage(sourceBuffer, { width, height, bgColor })
+      .then(buffer => writeSafe(src, buffer))
+  )));
 };
